8b5ec6de66c60e49937fba41860695f1fc18b6c4
codebreaker5163 <33778814+codebreaker5163@users.noreply.github.com>
2023-09-05 19:02:55 +0530

    Live Splash Demo

---
+++ LiveSplashDemo/LiveSplash.framework/Headers/LiveSplash.h
@@ -0,0 +1,18 @@
+//
+//  LiveSplash.h
+//  LiveSplash
+//
+//  Created by Himanshu Sharma on 05/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveSplash.
+FOUNDATION_EXPORT double LiveSplashVersionNumber;
+
+//! Project version string for LiveSplash.
+FOUNDATION_EXPORT const unsigned char LiveSplashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveSplash/PublicHeader.h>
+
+
--- LiveSplashDemo/ViewController.swift
+++ LiveSplashDemo/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
-
+import LiveSplash
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let splash = LiveSplash(size: self.view.frame, image: UIImage(named: "demoLiveSplash")!)
+        splash.startAnimation(delayInSec:15.0)
+        splash.addBlur(blurMode: .light)
+        self.view.addSubview(splash.animatedView())
+        self.view.sendSubviewToBack(splash.animatedView())
+        
     }
 
 
